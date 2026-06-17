721d8aff6a2fb7bcd3fb01d2911a81d066eda180
herminiosauceda0314 <micuentadosparami@gmail.com>
2026-06-17 12:25:22 -0600

    readyy

--- core/src/main/java/com/sokoban/game/GestorMusica.java
+++ core/src/main/java/com/sokoban/game/GestorMusica.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.sokoban.game;
+
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -18,32 +19,37 @@ public class GestorMusica {
     public static void iniciar(float volumen) {
         if (musica == null) {
             musica = Gdx.audio.newMusic(
-                Gdx.files.internal("musica/musica_fondo.ogg"));
+                    Gdx.files.internal("musica/musica_fondo.ogg"));
             musica.setLooping(true);
         }
         musica.setVolume(volumen);
-        if (activa) musica.play();
+        if (activa) {
+            musica.play();
+        }
     }
 
     public static void toggleMusica(ImageButton btnVolumen,
-                                     com.badlogic.gdx.graphics.Texture texOn,
-                                     com.badlogic.gdx.graphics.Texture texOff) {
+            com.badlogic.gdx.graphics.Texture texOn,
+            com.badlogic.gdx.graphics.Texture texOff) {
         activa = !activa;
         if (activa) {
             musica.play();
         } else {
             musica.pause();
         }
-        ((com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable)
-            btnVolumen.getStyle().imageUp)
-            .setRegion(new com.badlogic.gdx.graphics.g2d.TextureRegion(
-                activa ? texOn : texOff));
+        ((com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable) btnVolumen.getStyle().imageUp)
+                .setRegion(new com.badlogic.gdx.graphics.g2d.TextureRegion(
+                        activa ? texOn : texOff));
     }
 
-    public static boolean isActiva() { return activa; }
+    public static boolean isActiva() {
+        return activa;
+    }
 
     public static void setVolumen(float v) {
-        if (musica != null) musica.setVolume(v);
+        if (musica != null) {
+            musica.setVolume(v);
+        }
     }
 
     public static void dispose() {
